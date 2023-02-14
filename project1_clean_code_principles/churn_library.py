d2d91fb39f02d442d5a3b869d9f545c34fc4ce9b
Ashutosh_Singh8  <Ashutosh_Singh8@Dell.com>
2023-02-14 14:34:36 -0600

    modified churn_library.py, pylint score 8.8

--- project1_clean_code_principles/churn_library.py
+++ project1_clean_code_principles/churn_library.py
@@ -8,22 +8,22 @@ Date: 12th of February, 2023
 
 # Import Packages
 
+import os
+from sklearn.metrics import plot_roc_curve, classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 import joblib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
+import seaborn as sns
+sns.set()
 
-from sklearn.preprocessing import normalize
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import plot_roc_curve, classification_report
 
-import os
+os.environ['QT_QPA_PLATFORM'] = 'offscreen'
 
-os.environ['QT_QPA_PLATFORM']='offscreen'
 
 def import_data(pth):
     '''
@@ -36,14 +36,22 @@ def import_data(pth):
     '''
     return pd.read_csv(pth)
 
-def create_dir_struct(dir_list = ["images", "images/eda", "images/results", "logs", "models", "dataset_characteristics"]):
+
+def create_dir_struct(
+    dir_list=[
+        "images",
+        "images/eda",
+        "images/results",
+        "logs",
+        "models",
+        "dataset_characteristics"]):
     '''
     This function checks for required directory structure and creates the
     directories that are missing.
-    
+
     Adding dataset_characteristics to save outputs from the df.head(),
     df.shape, and  df.isnull.sum(), and df.describe().
-    
+
     input:
             dir_list: list of directory names
     output:
@@ -56,6 +64,7 @@ def create_dir_struct(dir_list = ["images", "images/eda", "images/results", "log
         else:
             continue
 
+
 def perform_eda(df):
     '''
     perform eda on df and save figures to images folder
@@ -67,22 +76,23 @@ def perform_eda(df):
     '''
     # Create the required directory strucutre
     create_dir_struct()
-    
+
     # Save dataset characteristics to files for manual viewing
     df.head().to_csv(r'./dataset_characteristics/df_head')
     np.savetxt(r'./dataset_characteristics/df_shape', df.shape, fmt='%d')
     df.isnull().sum().to_csv(r'./dataset_characteristics/df_null_counts')
     df.describe().to_csv(r'./dataset_characteristics/df_description')
-    
+
     # Create binary variable for Churn
     if 'Churn' not in df:
-        df['Churn'] = df['Attrition_Flag'].apply(lambda val: 0 if val=="Existing Customer" else 1)
-    
+        df['Churn'] = df['Attrition_Flag'].apply(
+            lambda val: 0 if val == "Existing Customer" else 1)
+
     # Create and save the binary churn distribution
     plt.figure(figsize=(20, 10))
     df['Attrition_Flag'].hist()
     plt.savefig(fname='./images/eda/churn_distribution.jpeg')
-    
+
     # Create and save customer age distribution
     plt.figure(figsize=(20, 10))
     df['Customer_Age'].hist()
@@ -100,11 +110,11 @@ def perform_eda(df):
 
     #  Create and save correlation heat map
     plt.figure(figsize=(20, 10))
-    sns.heatmap(df.corr(), annot=False, cmap='Dark2_r', linewidths = 2)
+    sns.heatmap(df.corr(), annot=False, cmap='Dark2_r', linewidths=2)
     plt.savefig(fname='./images/eda/heatmap.png')
 
 
-def encoder_helper(df, category_lst , response):
+def encoder_helper(df, category_lst, response):
     '''
     helper function to turn each categorical column into a new column with
     propotion of churn for each category - associated with cell 15 from the notebook
@@ -112,7 +122,8 @@ def encoder_helper(df, category_lst , response):
     input:
             df: pandas dataframe
             category_lst: list of columns that contain categorical features
-            response: string of response name [optional argument that could be used for naming variables or index y column]
+            response: string of response name.
+            [optional argument that could be used for naming variables or index y column]
 
     output:
             df: pandas dataframe with new columns for analysis
@@ -131,11 +142,13 @@ def encoder_helper(df, category_lst , response):
             df[category] = column_lst
     return df
 
-def perform_feature_engineering(df, response = 'Churn'):
+
+def perform_feature_engineering(df, response='Churn'):
     '''
     input:
               df: pandas dataframe
-              response: string of response name [optional argument that could be used for naming variables or index y column]
+              response: string of response name.
+              [optional argument that could be used for naming variables or index y column]
 
     output:
               X_train: X training data
@@ -145,33 +158,53 @@ def perform_feature_engineering(df, response = 'Churn'):
     '''
     # Creating binary variable 'Chrun', for cases when EDA is not performed
     if 'Churn' not in df:
-        df['Churn'] = df['Attrition_Flag'].apply(lambda val: 0 if val=="Existing Customer" else 1)
-    
+        df['Churn'] = df['Attrition_Flag'].apply(
+            lambda val: 0 if val == "Existing Customer" else 1)
+
     # categorical features
-    cat_columns = ['Gender', 'Education_Level', 'Marital_Status','Income_Category', 'Card_Category' ]
+    cat_columns = [
+        'Gender',
+        'Education_Level',
+        'Marital_Status',
+        'Income_Category',
+        'Card_Category']
 
     # feature engineering
     df = encoder_helper(df, cat_columns, response)
 
-    # target feature 
-    y = df['Churn']     
+    # target feature
+    y = df['Churn']
 
     # Create dataframe
-    X = pd.DataFrame()         
+    X = pd.DataFrame()
 
-    keep_cols = ['Customer_Age', 'Dependent_count', 'Months_on_book',
-                 'Total_Relationship_Count', 'Months_Inactive_12_mon',
-                 'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
-                 'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
-                 'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
-                 'Gender_Churn', 'Education_Level_Churn', 'Marital_Status_Churn',
-                 'Income_Category_Churn', 'Card_Category_Churn']
+    keep_cols = [
+        'Customer_Age',
+        'Dependent_count',
+        'Months_on_book',
+        'Total_Relationship_Count',
+        'Months_Inactive_12_mon',
+        'Contacts_Count_12_mon',
+        'Credit_Limit',
+        'Total_Revolving_Bal',
+        'Avg_Open_To_Buy',
+        'Total_Amt_Chng_Q4_Q1',
+        'Total_Trans_Amt',
+        'Total_Trans_Ct',
+        'Total_Ct_Chng_Q4_Q1',
+        'Avg_Utilization_Ratio',
+        'Gender_Churn',
+        'Education_Level_Churn',
+        'Marital_Status_Churn',
+        'Income_Category_Churn',
+        'Card_Category_Churn']
 
     # Features DataFrame
     X[keep_cols] = df[keep_cols]
 
     # Train and Test split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)  
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=42)
 
     return (X_train, X_test, y_train, y_test)
 
@@ -195,16 +228,26 @@ def classification_report_image(y_train,
              None
     '''
     plt.rc('figure', figsize=(5, 5))
-    plt.text(0.01, 1.25, str(model_name + ' Test'), {'fontsize': 10}, fontproperties = 'monospace')
-    plt.text(0.01, 0.05, str(classification_report(y_test, y_test_preds)), {'fontsize': 10}, fontproperties = 'monospace')
-    plt.text(0.01, 0.6, str(model_name +' Train'), {'fontsize': 10}, fontproperties = 'monospace')
-    plt.text(0.01, 0.7, str(classification_report(y_train, y_train_preds)), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.text(0.01, 1.25, str(model_name + ' Test'),
+             {'fontsize': 10}, fontproperties='monospace')
+    plt.text(
+        0.01, 0.05, str(
+            classification_report(
+                y_test, y_test_preds)), {
+            'fontsize': 10}, fontproperties='monospace')
+    plt.text(0.01, 0.6, str(model_name + ' Train'),
+             {'fontsize': 10}, fontproperties='monospace')
+    plt.text(
+        0.01, 0.7, str(
+            classification_report(
+                y_train, y_train_preds)), {
+            'fontsize': 10}, fontproperties='monospace')
     plt.axis('off')
-    
+
     result_file = result_dir + model_name + '_classf_rep' + '.png'
-    plt.savefig(fname= result_file)
-    
-    
+    plt.savefig(fname=result_file)
+
+
 def feature_importance_plot(model_file, X_data, output_pth):
     '''
     creates and stores the feature importances in pth
@@ -216,13 +259,13 @@ def feature_importance_plot(model_file, X_data, output_pth):
     output:
              None
     '''
-    
-    ## Putting a test to check if the clf has feature importance feature
+
+    # Putting a test to check if the clf has feature importance feature
     try:
         # Loading model
         model_obj = joblib.load(model_file)
         model_name = (model_file.split("/")[-1]).split(".")[0]
-        
+
         # Feature importances
         importances = model_obj.feature_importances_
 
@@ -247,9 +290,10 @@ def feature_importance_plot(model_file, X_data, output_pth):
 
         # Save the image
         plt.savefig(fname=output_pth + model_name + '_feature_importances.png')
-    
+
     except AttributeError:
         pass
+
 
 def roc_plotter(model, X_test, y_test, roc_file):
     '''
@@ -264,12 +308,12 @@ def roc_plotter(model, X_test, y_test, roc_file):
              None
     '''
     plt.figure(figsize=(15, 8))
-    display = plot_roc_curve(model,X_test, y_test)                              
-    plt.savefig(fname= roc_file)
+    plot_roc_curve(model, X_test, y_test)
+    plt.savefig(fname=roc_file)
     plt.close()
-    
 
-def train_models(df, model_dir, result_dir, clf, param_grid = None):
+
+def train_models(df, model_dir, result_dir, clf, param_grid=None):
     '''
     train, store model results: images + scores, and store models. Adding more function parameters
     to make this function more modular
@@ -287,66 +331,74 @@ def train_models(df, model_dir, result_dir, clf, param_grid = None):
     '''
     # Creating directory structure for cases when EDA is not performed
     create_dir_struct()
-    
+
     # Seperating Features and target variable, and performing train-test split
-    X_train, X_test, y_train, y_test = perform_feature_engineering(df, response = 'Churn')
-    
+    X_train, X_test, y_train, y_test = perform_feature_engineering(
+        df, response='Churn')
+
     # Checking for parameter grid, preforming grid search if the grid is provided
     # and fitting the classifier
-    if param_grid != None:
+    if param_grid is not None:
         model = GridSearchCV(estimator=clf, param_grid=param_grid, cv=5)
         model.fit(X_train, y_train)
         model = model.best_estimator_
-        
+
     else:
         model = clf.fit(X_train, y_train)
-    
+
     # Performing inference on feature sets
     y_train_preds = model.predict(X_train)
-    y_test_preds  = model.predict(X_test)
-    
-    # Generating model file name 
+    y_test_preds = model.predict(X_test)
+
+    # Generating model file name
     model_name = str(clf)
     model_file = model_dir + model_name + '.pkl'
     roc_file = result_dir + 'roc_curve_' + model_name + '.png'
-    
+
     # Saving the best model
     joblib.dump(model, model_file)
-    
+
     # Plotting and saving the ROC Curve
     roc_plotter(model, X_test, y_test, roc_file)
-    
+
     # Creating and saving the classification_report
     classification_report_image(y_train,
                                 y_test,
                                 y_train_preds,
                                 y_test_preds, model_name, result_dir)
-    
-    # Creating and saving the Feature Importance plot(for classifiers that support this option)
+
+    # Creating and saving the Feature Importance plot(for classifiers that
+    # support this option)
     feature_importance_plot(model_file, X_test, result_dir)
-    
+
 
 if __name__ == '__main__':
     # Import data
     BANK_DF = import_data(pth='./data/bank_data.csv')
 
-    # Perform EDA 
-    EDA_DF = perform_eda(df=BANK_DF)
-    
+    # Perform EDA
+    perform_eda(df=BANK_DF)
+
     # Initializing (classifier, parameter grid) tuple for sample runs
-    clf_paramGrid = [(RandomForestClassifier(random_state=42),{'n_estimators': [200, 500],
-                  'max_features': ['auto', 'sqrt'],
-                  'max_depth' : [4, 5, 100],
-                  'criterion' :['gini', 'entropy']}),(LogisticRegression(solver='lbfgs', max_iter=3000),None)]
-    
+    clf_paramGrid = [
+        (RandomForestClassifier(
+            random_state=42), {
+            'n_estimators': [
+                200, 500], 'max_features': [
+                    'auto', 'sqrt'], 'max_depth': [
+                        4, 5, 100], 'criterion':[
+                            'gini', 'entropy']}), (LogisticRegression(
+                                solver='lbfgs', max_iter=3000), None)]
+
     # Initializing directories to save models and results
-    model_dir = r'./models/'
-    result_dir = r'./images/results/'
-    
+    MODEL_DIR = r'./models/'
+    RESULT_DIR = r'./images/results/'
+
     # Sample runs
-    for clf, param_grid in clf_paramGrid:
-        train_models(df = BANK_DF,model_dir = model_dir , result_dir = result_dir,clf=clf,param_grid = param_grid)
-        
-    
-    
-    
+    for classif, grid in clf_paramGrid:
+        train_models(
+            df=BANK_DF,
+            model_dir=MODEL_DIR,
+            result_dir=RESULT_DIR,
+            clf=classif,
+            param_grid=grid)
